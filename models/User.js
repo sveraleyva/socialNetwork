28bfb42fc11545cfca28f59ba3ad4cf18263abca
Samantha Leyva <samanthaleyva@Samanthas-MacBook-Air-2.local>
2023-04-15 22:38:22 -0500

    created file structure, added connection.js and started User model

---
+++ models/User.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: [/^\S+@\S+\.\S+$/, "Please enter a valid email address"],
+  },
+  thoughts: [{ type: Schema.Types.ObjectId, ref: "Thought" }],
+  friends: [{ type: Schema.Types.ObjectId, ref: "User" }],
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
